dbb46798bdbd0c0d179caf0bc1ee42bee1ab13b0
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-08-04 20:47:08 +0300

    fix deploy

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const router = require('./routes/index');
 const { BD_URL } = require('./utils/config');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(helmet());
 app.disable('x-powered-by');
 
+app.use(cors);
 app.use(cookieParser());
 app.use(bodyParser.json());
 mongoose.connect(BD_URL);
